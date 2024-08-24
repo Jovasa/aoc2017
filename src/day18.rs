579b0cad45ef3a9db374fd1d07b4f2d787b0db82
Jovasa <jovasa46@gmail.com>
2024-08-24 11:05:35 +0300

    Dy 18 part 2

--- src/day18.rs
+++ src/day18.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 fn get_value(registers: &HashMap<String, i64>, value: &str) -> i64 {
     if let Ok(number) = value.parse::<i64>() {
@@ -12,22 +12,44 @@ fn main() {
     let input = std::fs::read_to_string("data/day18.txt").expect("Unable to read file");
     let instructions: Vec<Vec<&str>> = input.lines().map(|x| x.split_whitespace().collect()).collect();
 
-    let mut registers: HashMap<String, i64> = HashMap::new();
-    let mut last_sound = 0;
-    let mut position = 0;
+    let mut registers0: HashMap<String, i64> = HashMap::new();
+    registers0.insert("p".to_string(), 0);
+    let mut sends = 0;
+    let mut position0 = 0;
+    let mut out_stack0: VecDeque<i64> = VecDeque::new();
+
+    let mut registers1: HashMap<String, i64> = HashMap::new();
+    registers1.insert("p".to_string(), 1);
+    let mut position1 = 0;
+    let mut out_stack1: VecDeque<i64> = VecDeque::new();
+
     loop {
-        if position >= instructions.len() {
+        if position0 >= instructions.len() || position1 >= instructions.len() {
             break;
         }
-        if do_one_instruction(&instructions, &mut registers, &mut last_sound, &mut position) { break; }
+        if instructions[position1][0] == "snd" {
+            sends += 1;
+        }
+        if let Some(value) = do_one_instruction(&instructions, &mut registers0, &mut out_stack1, &mut position0) {
+            out_stack0.push_back(value);
+        }
+        if let Some(value) = do_one_instruction(&instructions, &mut registers1, &mut out_stack0, &mut position1) {
+            out_stack1.push_back(value);
+        }
+        if instructions[position0][0] == "rcv" && instructions[position1][0] == "rcv" && out_stack0.is_empty() && out_stack1.is_empty() {
+            break;
+        }
     }
+    println!("Part 2: {}", sends);
 }
 
-fn do_one_instruction(instructions: &Vec<Vec<&str>>, registers: &mut HashMap<String, i64>, last_sound: &mut i64, position: &mut usize) -> bool {
+fn do_one_instruction(instructions: &Vec<Vec<&str>>, registers: &mut HashMap<String, i64>, last_sound: &mut VecDeque<i64>, position: &mut usize) -> Option<i64> {
     let instruction = &instructions[*position];
     match instruction[0] {
         "snd" => {
-            *last_sound = get_value(&registers, instruction[1]);
+            let temp =  get_value(&registers, instruction[1]);
+            *position += 1;
+            return Some(temp);
         },
         "set" => {
             let value = get_value(&registers, instruction[2]);
@@ -46,9 +68,10 @@ fn do_one_instruction(instructions: &Vec<Vec<&str>>, registers: &mut HashMap<Str
             *registers.entry(instruction[1].to_string()).or_insert(0) %= value;
         },
         "rcv" => {
-            if get_value(&registers, instruction[1]) != 0 {
-                println!("Part 1: {}", last_sound);
-                return true;
+            if let Some(value) = last_sound.pop_front() {
+                registers.insert(instruction[1].to_string(), value);
+            } else {
+                return None;
             }
         },
         "jgz" => {
@@ -59,5 +82,5 @@ fn do_one_instruction(instructions: &Vec<Vec<&str>>, registers: &mut HashMap<Str
         _ => panic!("Unknown instruction"),
     }
     *position += 1;
-    false
+    None
 }
